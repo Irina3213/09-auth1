266c20469c571f7ba6d5c8fed09ef23ed5f47d45
Iryna Yeremizina <yeremizina1@gmail.com>
2025-12-28 17:18:37 +0200

    changes

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -6,14 +6,15 @@ import {
 } from "@tanstack/react-query";
 import { fetchServerNotes } from "@/lib/api/serverApi";
 import { Metadata } from "next";
-import { cookies } from "next/headers";
+
+
 
 type Props = {
-  params: Promise<{ slug: string[] }>; // ✅ params — Promise
+  params: Promise<{ slug: string[] }>;
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { slug } = await params; // ✅ чекаємо на Promise
+  const { slug } = await params;
   const tag = slug[0];
 
   const title = `Notes filtered by: ${tag}`;
@@ -43,16 +44,15 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function App({ params }: Props) {
   const { slug } = await params;
+  
   const tag = slug[0] === "All" ? undefined : slug[0];
 
   const queryClient = new QueryClient();
-  const cookieStore = await cookies(); // Отримуємо куки
+
 
   await queryClient.prefetchQuery({
     queryKey: ["notes", { query: "", page: 1, tag }],
-    // Передаємо об'єкт { page, search, tag } та рядок кук
-    queryFn: () =>
-      fetchServerNotes({ page: 1, search: "", tag }, cookieStore.toString()),
+    queryFn: () => fetchServerNotes({ page: 1, search: "", tag }),
   });
 
   return (
@@ -63,3 +63,4 @@ export default async function App({ params }: Props) {
     </div>
   );
 }
+
