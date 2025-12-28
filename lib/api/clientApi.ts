89a3549ecf9477f8ca0c5c8ae2ac35686189048e
Iryna Yeremizina <yeremizina1@gmail.com>
2025-12-28 17:47:26 +0200

    changes mistake

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -21,30 +21,39 @@ export interface NoteResponse {
   page: number;
 }
 
+// Реєстрація
 export const register = async (data: RegisterCredentials): Promise<User> => {
   const res = await instance.post("/auth/register", data);
   return res.data;
 };
 
+// Логін
 export const login = async (data: LoginCredentials): Promise<User> => {
   const res = await instance.post("/auth/login", data);
   return res.data;
 };
 
+// Логаут
 export const logout = async (): Promise<void> => {
   await instance.post("/auth/logout");
 };
 
 export const checkSession = async (): Promise<User | null> => {
-  const res = await instance.get("/auth/session");
-  return res.data || null;
+  try {
+    const res = await instance.get("/auth/session");
+    return res.data || null;
+  } catch {
+    return null;
+  }
 };
 
+// Профіль
 export const updateMe = async (data: UpdateUserDto): Promise<User> => {
   const res = await instance.patch("/users/me", data);
   return res.data;
 };
 
+// Нотатки
 export const createNote = async (data: CreateNoteParams): Promise<Note> => {
   const res = await instance.post("/notes", data);
   return res.data;
