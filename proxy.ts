f7c4919b5ebda62a7bf0beb520783f28dbf0efe1
Iryna Yeremizina <yeremizina1@gmail.com>
2025-12-30 15:59:32 +0200

    changes 19

--- proxy.ts
+++ proxy.ts
@@ -19,26 +19,45 @@ export async function proxy(request: NextRequest) {
   const isAuthRoute = authRoutes.some((route) => pathname.startsWith(route));
 
   let isAuthenticated = !!accessToken;
+
   const res = NextResponse.next();
 
+  // 1. Оновлення сесії
   if (!accessToken && refreshToken) {
     try {
-      const session = await checkSession();
+      const user = await checkSession();
 
-      if (session) {
+      if (user) {
         isAuthenticated = true;
+
+        const updatedCookies = await cookies();
+        const newAccess = updatedCookies.get("accessToken")?.value;
+        const newRefresh = updatedCookies.get("refreshToken")?.value;
+
+        if (newAccess) res.cookies.set("accessToken", newAccess);
+        if (newRefresh) res.cookies.set("refreshToken", newRefresh);
       }
     } catch (error) {
-      console.error("Session check failed:", error);
+      console.error("Session refresh failed", error);
     }
   }
 
+  // 2. Логіка перенаправлень
   if (isPrivateRoute && !isAuthenticated) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
   if (isAuthRoute && isAuthenticated) {
-    return NextResponse.redirect(new URL("/", request.url));
+    const redirectRes = NextResponse.redirect(new URL("/", request.url));
+
+    const updatedCookies = await cookies();
+    const newAccess = updatedCookies.get("accessToken")?.value;
+    const newRefresh = updatedCookies.get("refreshToken")?.value;
+
+    if (newAccess) redirectRes.cookies.set("accessToken", newAccess);
+    if (newRefresh) redirectRes.cookies.set("refreshToken", newRefresh);
+
+    return redirectRes;
   }
 
   return res;
