106727ea188633254b7d4ed7ffac4f8ebd487aa4
Iryna Yeremizina <yeremizina1@gmail.com>
2025-12-31 19:44:04 +0200

    changes 22-16

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,8 +1,15 @@
-import axios from "axios";
+// import axios from "axios";
 
 // const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
 
+// export const instance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL || "https://ваш-бекент.com/api",
+//   withCredentials: true,
+// });
+
+import axios from "axios";
+
 export const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://ваш-бекент.com/api",
+  baseURL: "",
   withCredentials: true,
 });
--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    proxy: true,
+  },
   async rewrites() {
     return [
       {
-        // Всі запити, що починаються на /auth, йдуть на Railway
         source: "/auth/:path*",
         destination:
           "https://auth-backend-production-c662.up.railway.app/auth/:path*",
       },
       {
-        // Всі запити, що починаються на /api, йдуть на Railway
         source: "/api/:path*",
         destination:
           "https://auth-backend-production-c662.up.railway.app/api/:path*",
@@ -18,4 +19,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
